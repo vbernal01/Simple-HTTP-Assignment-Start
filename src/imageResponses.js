6d2f31ae0c4ba24ff80f513866a876a2b9a3f128
Vicente Bernal <vb9288@rit.edu>
2022-09-04 16:39:07 -0400

    assignment 1 completed

---
+++ src/imageResponses.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const image = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getImage = (request, response) => {
+  response.writeHead(200,{'Content-Type': 'image/png'});
+  response.write(image);
+  response.end();
+};
+
+module.exports.getImage = getImage;
